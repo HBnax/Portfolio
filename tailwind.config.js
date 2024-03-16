252f45c072d7339fa55c1e11382608dca8d4bb64
Halle Broadnax <broadnax@chapman.edu>
2024-03-15 22:48:43 -0700

    adding files to portfolio repo

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        }
+      },
+      fontFamily: {
+        body: ['Nunito']
+      },
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+
+    },
+  },
+  plugins: [],
+}
+
